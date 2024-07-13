4d997260cfaa217d3f9f4113c3db062fb5912a20
toshikisugiyama <s.toshiki.llwz8869@gmail.com>
2024-07-13 16:45:50 +0900

    add: bpmf

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,71 @@
-import Image from "next/image";
-
 export default function Home() {
+  type TItem = {
+    id: string;
+    bpmf: string;
+    pinyin: string;
+    category: 'vowel' | 'consonant';
+  };
+
+  const list: TItem[] = [
+    { id: '01', bpmf: 'ㄅ', pinyin: 'b', category: 'consonant' },
+    { id: '02', bpmf: 'ㄆ', pinyin: 'p', category: 'consonant' },
+    { id: '03', bpmf: 'ㄇ', pinyin: 'm', category: 'consonant' },
+    { id: '04', bpmf: 'ㄈ', pinyin: 'f', category: 'consonant' },
+    { id: '05', bpmf: 'ㄉ', pinyin: 'd', category: 'consonant' },
+    { id: '06', bpmf: 'ㄊ', pinyin: 't', category: 'consonant' },
+    { id: '07', bpmf: 'ㄋ', pinyin: 'n', category: 'consonant' },
+    { id: '08', bpmf: 'ㄌ', pinyin: 'l', category: 'consonant' },
+    { id: '09', bpmf: 'ㄍ', pinyin: 'g', category: 'consonant' },
+    { id: '10', bpmf: 'ㄎ', pinyin: 'k', category: 'consonant' },
+    { id: '11', bpmf: 'ㄏ', pinyin: 'h', category: 'consonant' },
+    { id: '12', bpmf: 'ㄐ', pinyin: 'j', category: 'consonant' },
+    { id: '13', bpmf: 'ㄑ', pinyin: 'q', category: 'consonant' },
+    { id: '14', bpmf: 'ㄒ', pinyin: 'x', category: 'consonant' },
+    { id: '15', bpmf: 'ㄓ', pinyin: 'zh', category: 'consonant' },
+    { id: '16', bpmf: 'ㄔ', pinyin: 'ch', category: 'consonant' },
+    { id: '17', bpmf: 'ㄕ', pinyin: 'sh', category: 'consonant' },
+    { id: '18', bpmf: 'ㄖ', pinyin: 'r', category: 'consonant' },
+    { id: '19', bpmf: 'ㄗ', pinyin: 'z', category: 'consonant' },
+    { id: '20', bpmf: 'ㄘ', pinyin: 'c', category: 'consonant' },
+    { id: '21', bpmf: 'ㄙ', pinyin: 's', category: 'consonant' },
+    { id: '22', bpmf: 'ㄚ', pinyin: 'a', category: 'vowel' },
+    { id: '23', bpmf: 'ㄛ', pinyin: 'o', category: 'vowel' },
+    { id: '24', bpmf: 'ㄜ', pinyin: 'e', category: 'vowel' },
+    { id: '25', bpmf: 'ㄝ', pinyin: 'ê', category: 'vowel' },
+    { id: '26', bpmf: 'ㄞ', pinyin: 'ai', category: 'vowel' },
+    { id: '27', bpmf: 'ㄟ', pinyin: 'ei', category: 'vowel' },
+    { id: '28', bpmf: 'ㄠ', pinyin: 'ao', category: 'vowel' },
+    { id: '29', bpmf: 'ㄡ', pinyin: 'ou', category: 'vowel' },
+    { id: '30', bpmf: 'ㄢ', pinyin: 'an', category: 'vowel' },
+    { id: '31', bpmf: 'ㄣ', pinyin: 'en', category: 'vowel' },
+    { id: '32', bpmf: 'ㄤ', pinyin: 'ang', category: 'vowel' },
+    { id: '33', bpmf: 'ㄥ', pinyin: 'eng', category: 'vowel' },
+    { id: '34', bpmf: 'ㄦ', pinyin: 'er', category: 'vowel' },
+    { id: '35', bpmf: 'ㄧ', pinyin: 'i/y', category: 'vowel' },
+    { id: '36', bpmf: 'ㄨ', pinyin: 'u/w', category: 'vowel' },
+    { id: '37', bpmf: 'ㄩ', pinyin: 'ü/yu', category: 'vowel' },
+    { id: '38', bpmf: 'ㄭ', pinyin: '-i', category: 'vowel' },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <div className="mx-auto max-w-2xl p-4">
+        <ul className="no-scrollbar flex aspect-square w-full snap-x snap-mandatory items-start justify-start gap-2 overflow-auto">
+          {list.map((item, index) => (
+            <li
+              key={item.id}
+              className="relative flex aspect-square w-full shrink-0 snap-center flex-col items-center justify-center gap-4 rounded-2xl bg-cyan-900 font-serif text-[15rem] font-semibold leading-none text-white"
+            >
+              {item.bpmf}
+              <span className="absolute left-[15%] top-[14%] -translate-x-1/2 -translate-y-1/2 text-2xl font-normal">
+                No.{index + 1}
+              </span>
+              <span className="absolute bottom-[15%] left-1/2 -translate-x-1/2 translate-y-1/2 text-2xl">
+                {item.pinyin}
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
